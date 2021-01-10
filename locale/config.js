4a8146045a90933c24f948c8457dfeda633938d2
Ramon Guilherme <ramonguilherme@outlook.com>
2021-01-10 19:29:07 -0300

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,44 +1,20 @@
-moment.locale('pt-br', {
-	months : 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split('_'),
-	monthsShort : 'Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez'.split('_'),
-	weekdays : 'Domingo_Segunda-Feira_Terça-Feira_Quarta-Feira_Quinta-Feira_Sexta-Feira_Sábado'.split('_'),
-	weekdaysShort : 'Dom_Seg_Ter_Qua_Qui_Sex_Sáb'.split('_'),
-	weekdaysMin : 'Dom_2ª_3ª_4ª_5ª_6ª_Sáb'.split('_'),
-	longDateFormat : {
-		LT : 'HH:mm',
-		LTS : 'HH:mm:ss',
-		L : 'DD/MM/YYYY',
-		LL : 'D [de] MMMM [de] YYYY',
-		LLL : 'D [de] MMMM [de] YYYY [às] HH:mm',
-		LLLL : 'dddd, D [de] MMMM [de] YYYY [às] HH:mm'
-	},
-	calendar : {
-		sameDay: '[Hoje às] LT',
-		nextDay: '[Amanhã às] LT',
-		nextWeek: 'dddd [às] LT',
-		lastDay: '[Ontem às] LT',
-		lastWeek: function () {
-		return (this.day() === 0 || this.day() === 6) ?
-		'[Último] dddd [às] LT' : // Saturday + Sunday
-		'[Última] dddd [às] LT'; // Monday - Friday
-	},
-	sameElse: 'L'
-	},
-	relativeTime : {
-		future : 'em %s',
-		past : '%s atrás',
-		s : 'poucos segundos',
-		m : 'um minuto',
-		mm : '%d minutos',
-		h : 'uma hora',
-		hh : '%d horas',
-		d : 'um dia',
-		dd : '%d dias',
-		M : 'um mês',
-		MM : '%d meses',
-		y : 'um ano',
-		yy : '%d anos'
-	},
-	ordinalParse: /\d{1,2}º/,
-	ordinal : '%dº'
+!(function (e, o) {
+    "object" == typeof exports && "undefined" != typeof module ? (module.exports = o(require("dayjs"))) : "function" == typeof define && define.amd ? define(["dayjs"], o) : (e.dayjs_locale_pt_br = o(e.dayjs));
+})(this, function (e) {
+    "use strict";
+    e = e && e.hasOwnProperty("default") ? e.default : e;
+    var o = {
+        name: "pt-br",
+        weekdays: "Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado".split("_"),
+        weekdaysShort: "Dom_Seg_Ter_Qua_Qui_Sex_Sáb".split("_"),
+        weekdaysMin: "Do_2ª_3ª_4ª_5ª_6ª_Sá".split("_"),
+        months: "Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro".split("_"),
+        monthsShort: "Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez".split("_"),
+        ordinal: function (e) {
+            return e + "º";
+        },
+        formats: { LT: "HH:mm", LTS: "HH:mm:ss", L: "DD/MM/YYYY", LL: "D [de] MMMM [de] YYYY", LLL: "D [de] MMMM [de] YYYY [às] HH:mm", LLLL: "dddd, D [de] MMMM [de] YYYY [às] HH:mm" },
+        relativeTime: { future: "em %s", past: "há %s", s: "poucos segundos", m: "um minuto", mm: "%d minutos", h: "uma hora", hh: "%d horas", d: "um dia", dd: "%d dias", M: "um mês", MM: "%d meses", y: "um ano", yy: "%d anos" },
+    };
+    return e.locale(o, null, !0), o;
 });
